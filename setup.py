3cb33dbba3f98abea83b3f13ade7a1d9e8a22179
Nathaniel Smith <njs@pobox.com>
2009-12-12 02:01:38 -0800

    initial scikits skeleton

---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+
+# Copyright (C) 2008 Cournapeau David <cournape@gmail.com>
+# Copyright (C) 2009 Nathaniel Smith <njs@pobox.com>
+
+descr   = """Sparse matrix tools.
+
+This is a home for sparse matrix code in Python that plays well with
+scipy.sparse, but that is somehow unsuitable for inclusion in scipy
+proper. Usually this will be because it is released under the GPL.
+
+So far we have a wrapper for the CHOLMOD library for sparse cholesky
+decomposition. Further contributions are welcome!
+"""
+
+import os
+import sys
+
+DISTNAME            = 'scikits.sparse'
+DESCRIPTION         = 'Scikits sparse matrix package'
+LONG_DESCRIPTION    = descr
+MAINTAINER          = 'Nathaniel Smith',
+MAINTAINER_EMAIL    = 'njs@pobox.com',
+URL                 = 'http://code.google.com/p/scikits-sparse/'
+LICENSE             = 'GPL'
+DOWNLOAD_URL        = "http://code.google.com/p/scikits-sparse/downloads/list"
+VERSION             = '0.0.0'
+
+import setuptools
+from numpy.distutils.core import setup
+
+def configuration(parent_package='', top_path=None, package_name=DISTNAME):
+    if os.path.exists('MANIFEST'): os.remove('MANIFEST')
+
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(package_name, parent_package, top_path,
+                           version = VERSION,
+                           maintainer  = MAINTAINER,
+                           maintainer_email = MAINTAINER_EMAIL,
+                           description = DESCRIPTION,
+                           license = LICENSE,
+                           url = URL,
+                           download_url = DOWNLOAD_URL,
+                           long_description = LONG_DESCRIPTION)
+
+    return config
+
+if __name__ == "__main__":
+    setup(configuration = configuration,
+        install_requires = 'numpy',
+        namespace_packages = ['scikits'],
+        packages = setuptools.find_packages(),
+        include_package_data = True,
+        #test_suite="tester", # for python setup.py test
+        zip_safe = False, # the package can run out of an .egg file
+        classifiers =
+            [ 'Development Status :: 3 - Alpha',
+              'Environment :: Console',
+              'Intended Audience :: Developers',
+              'Intended Audience :: Science/Research',
+              'License :: OSI Approved :: GNU General Public License (GPL)',
+              'Topic :: Scientific/Engineering'])
