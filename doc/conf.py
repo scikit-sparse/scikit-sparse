b29d98d3ebae0597160f246504c4c48cac2f379e
Nathaniel Smith <njs@pobox.com>
2009-12-14 02:44:28 -0800

    add +dev to versions and make some fixes to README and release-checklist

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@ copyright = u'2009, Nathaniel Smith'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.1+dev'
 # The full version, including alpha/beta/rc tags.
 release = version
 
