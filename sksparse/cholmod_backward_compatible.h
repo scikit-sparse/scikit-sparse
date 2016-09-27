c99284145ae02b94606440e7957376342e755f93
Joscha Reimer <jor@informatik.uni-kiel.de>
2016-09-27 09:26:37 +0200

    use ifndef in cholmod_backward_compatible.h

--- sksparse/cholmod_backward_compatible.h
+++ sksparse/cholmod_backward_compatible.h
@@ -2,6 +2,6 @@
 
 // CHOLMOD_GPU_PROBLEM is only defined since version 2.0 of cholmod,
 // so we need to define it here for backward compatibility
-#if CHOLMOD_MAIN_VERSION < 2
+#ifndef CHOLMOD_GPU_PROBLEM
 #define CHOLMOD_GPU_PROBLEM -5
 #endif
