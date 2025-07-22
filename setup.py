3a22e6a2358984e92400000fefa25f17644ad115
Bernard Roesler <bernard.roesler@gmail.com>
2025-07-22 15:26:43 -0400

    DEV: Update to PEP 621 pyproject.toml style. Version -> 0.5.0.

--- setup.py
+++ setup.py
@@ -11,16 +11,6 @@
 # 2021-       Justin Ellis            <justin.ellis18@gmail.com>
 # 2022-       Aaron Johnson           <aaron9035@gmail.com>
 
-"""Sparse matrix tools.
-
-This is a home for sparse matrix code in Python that plays well with
-scipy.sparse, but that is somehow unsuitable for inclusion in scipy
-proper. Usually this will be because it is released under the GPL.
-
-So far we have a wrapper for the CHOLMOD library for sparse Cholesky
-decomposition. Further contributions are welcome!
-"""
-
 import os
 import subprocess
 import sys
@@ -28,14 +18,6 @@ import sys
 import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, find_packages, setup
-
-DISTNAME = "scikit-sparse"
-DESCRIPTION = "Scikit sparse matrix package"
-LONG_DESCRIPTION = __doc__
-MAINTAINER = "Aaron Johnson"
-MAINTAINER_EMAIL = "aaron9035@gmail.com"
-URL = "https://github.com/scikit-sparse/scikit-sparse"
-LICENSE = "BSD"
 
 INCLUDE_DIRS = [
     np.get_include(),
@@ -55,7 +37,9 @@ homebrew_suitesparse_dir = (
     .stdout.decode()
     .strip()
 )
-if homebrew_suitesparse_dir:  # empty string if not found (because error is printed to stderr)
+
+# empty string if not found (because error is printed to stderr)
+if homebrew_suitesparse_dir:
     INCLUDE_DIRS.append(
         # Include directory for homebrew-installed suitesparse
         homebrew_suitesparse_dir
@@ -69,6 +53,7 @@ if homebrew_suitesparse_dir:  # empty string if not found (because error is prin
 
 user_include_dir = os.getenv("SUITESPARSE_INCLUDE_DIR")
 user_library_dir = os.getenv("SUITESPARSE_LIBRARY_DIR")
+
 if user_include_dir:
     INCLUDE_DIRS.append(user_include_dir)
 
@@ -76,38 +61,10 @@ if user_library_dir:
     LIBRARY_DIRS.append(user_library_dir)
 
 setup(
-    install_requires=["numpy>=1.13.3", "scipy>=0.19"],
-    python_requires=">=3.6",
     packages=find_packages(),
     package_data={
         "": ["test_data/*.mtx.gz"],
     },
-    name=DISTNAME,
-    version="0.4.16",  # remember to update __init__.py
-    maintainer=MAINTAINER,
-    maintainer_email=MAINTAINER_EMAIL,
-    description=DESCRIPTION,
-    license=LICENSE,
-    url=URL,
-    long_description=LONG_DESCRIPTION,
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Cython",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Mathematics",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-    ],
     # You may specify the directory where CHOLMOD is installed using the
     # library_dirs and include_dirs keywords in the lines below.
     ext_modules=cythonize(
