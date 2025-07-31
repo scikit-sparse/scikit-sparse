e45f03368cbe74c048b16ee2256dd490e00a630c
Bernard Roesler <bernard.roesler@gmail.com>
2025-07-31 10:47:33 -0400

    ENH: Create colamd module. Basic tests pass.
    Includes steps:
    * Creating .pxd and .pyx files
    * Add colamd to setup.py and sksparse/__init__.py.
    * Refactor test helper functions.
        Move is_valid_permutation and generate_random_matrices into
        tests/helpers.py and touch tests/__init__.py to allow imports.

---
+++ sksparse/colamd.pxd
@@ -0,0 +1,98 @@
+# Cython COLAMD header interface
+#
+# Part of the scikit-sparse project.
+# Copyright (C) 2025 Bernard Roesler. All rights reserved.
+# See pyproject.toml for full author list and LICENSE.txt for license details.
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# =============================================================================
+#     File: colamd.pxd
+#  Created: 2025-07-31 09:45
+# =============================================================================
+# distutils: language = c
+# cython: language_level=3
+
+from libc.stddef cimport size_t
+from libc.stdint cimport int32_t, int64_t
+
+
+cdef extern from "colamd.h":
+    # sizes of input and output arrays
+    int COLAMD_KNOBS
+    int COLAMD_STATS
+
+    # indices of knobs
+    int COLAMD_DENSE_ROW
+    int COLAMD_DENSE_COL
+    int COLAMD_AGGRESSIVE
+
+    # indices of stats
+    int COLAMD_DEFRAG_COUNT
+    int COLAMD_STATUS
+    int COLAMDINFO1
+    int COLAMDINFO2
+    int COLAMDINFO3
+
+    # return values of colamd
+    int COLAMD_OK
+    # int COLAMD_OK_BUT_JUMBLED  # not used in scikit-sparse
+    int COLAMD_ERROR_A_not_present
+    int COLAMD_ERROR_p_not_present
+    int COLAMD_ERROR_nrow_negative
+    int COLAMD_ERROR_ncol_negative
+    int COLAMD_ERROR_nnz_negative
+    int COLAMD_ERROR_p0_nonzero
+    int COLAMD_ERROR_A_too_small
+    int COLAMD_ERROR_col_length_negative
+    int COLAMD_ERROR_row_index_out_of_bounds
+    int COLAMD_ERROR_out_of_memory
+    int COLAMD_ERROR_internal_error
+
+    size_t colamd_recommended(int32_t nnz, int32_t n_row, int32_t n_col)
+    size_t colamd_l_recommended(int64_t nnz, int64_t n_row, int64_t n_col)
+
+    void colamd_set_defaults(double knobs[])
+    void colamd_l_set_defaults(double knobs[])
+
+    int c_colamd "colamd"(
+        int32_t n_row,
+        int32_t n_col,
+        int32_t Alen,
+        int32_t A[],
+        int32_t p[],
+        double knobs[],
+        int32_t stats[]
+    )
+
+    int c_colamd_l "colamd_l"(
+        int64_t n_row,
+        int64_t n_col,
+        int64_t Alen,
+        int64_t A[],
+        int64_t p[],
+        double knobs[],
+        int64_t stats[]
+    )
+
+    # int symamd(
+    #     int32_t n,
+    #     int32_t A[],
+    #     int32_t p[],
+    #     int32_t perm[],
+    #     double knobs[COLAMD_KNOBS],
+    #     int32_t stats[COLAMD_STATS],
+    #     void * (*allocate) (size_t, size_t),
+    #     void (*release) (void *)
+    # )
+
+    # int symamd_l
+    # (
+    #     int64_t n,
+    #     int64_t A[],
+    #     int64_t p[],
+    #     int64_t perm[],
+    #     double knobs[COLAMD_KNOBS],
+    #     int64_t stats[COLAMD_STATS],
+    #     void * (*allocate) (size_t, size_t),
+    #     void (*release) (void *)
+    # )
---
+++ tests/test_colamd.py
@@ -0,0 +1,149 @@
+# Test cases for the sksparse.colamd module.
+#
+# Part of the scikit-sparse project.
+# Copyright (C) 2025 Bernard Roesler
+# See pyproject.toml for full author list and LICENSE.txt for license details.
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# =============================================================================
+#     File: test_colamd.py
+#  Created: 2025-07-31 11:42
+# =============================================================================
+
+"""Test cases for the sksparse.colamd module."""
+
+import numpy as np
+import pytest
+
+from numpy.testing import assert_array_equal
+# from pathlib import Path
+from scipy import sparse
+from scipy.sparse import SparseEfficiencyWarning
+from sksparse.colamd import colamd
+
+# from .helpers import is_valid_permutation, generate_random_matrices
+
+
+@pytest.mark.parametrize("itype", [np.int32, np.int64])
+def test_empty_input(itype):
+    empty_A = sparse.csc_array((0, 0))
+    empty_A.indptr = empty_A.indptr.astype(itype)
+    empty_A.indices = empty_A.indices.astype(itype)
+    assert_array_equal(colamd(empty_A), np.array([], dtype=itype), strict=True)
+
+
+def test_1D_row_input():
+    with pytest.raises(ValueError, match="must be 2D"):
+        colamd(np.arange(10))
+
+
+def test_2D_row_input():
+    with pytest.warns(SparseEfficiencyWarning, match="not in CSC format"):
+        N = 10
+        p = colamd(np.arange(N)[np.newaxis, :])  # (1, N)
+        assert_array_equal(p, np.arange(N, dtype=np.int32), strict=True)
+
+
+def test_2D_col_input():
+    with pytest.warns(SparseEfficiencyWarning, match="not in CSC format"):
+        N = 10
+        p = colamd(np.arange(N)[:, np.newaxis])  # (N, 1)
+        assert_array_equal(p, np.zeros(1, dtype=np.int32), strict=True)
+
+
+def test_ND_input():
+    rng = np.random.default_rng(565656)
+    with pytest.raises(ValueError, match="must be 2D"):
+        colamd(rng.random((2, 3, 4)))
+
+
+@pytest.mark.parametrize("itype", [np.int32, np.int64])
+def test_zero_input(itype):
+    N = 10  # arbitrary
+    zero_A = sparse.csc_array((N, N))
+    zero_A.indptr = zero_A.indptr.astype(itype)
+    zero_A.indices = zero_A.indices.astype(itype)
+    assert_array_equal(colamd(zero_A), np.arange(N, dtype=itype), strict=True)
+
+
+def test_singleton_matrix():
+    singleton_A = sparse.csc_array([[1]])
+    assert_array_equal(colamd(singleton_A), np.array([0], dtype=np.int32), strict=True)
+
+
+# @pytest.mark.parametrize(
+#     "A", list(generate_random_matrices(N_trials=100, N_max=200, d_scale=0.05)),
+# )
+# class TestRandomSquareMatrices:
+#     @pytest.mark.parametrize("matrix_type", ["dense", "csc", "coo"])
+#     def test_input_type(self, A, matrix_type):
+#         match matrix_type:
+#             case "dense":
+#                 A = A.toarray()
+#             case "csc":
+#                 A = A.tocsc()
+#             case "coo":
+#                 A = A.tocoo()
+#             case _:
+#                 raise ValueError(f"Unknown matrix type: {matrix_type}")
+
+#         if matrix_type != "csc":
+#             with pytest.warns(SparseEfficiencyWarning, match="not in CSC format"):
+#                 p = colamd(A)
+#         else:
+#             p = colamd(A)
+
+#         assert is_valid_permutation(p)
+
+#     @pytest.mark.parametrize("itype", [np.int32, np.int64])
+#     def test_itype(self, A, itype):
+#         A.indptr = A.indptr.astype(itype)
+#         A.indices = A.indices.astype(itype)
+#         p = colamd(A)
+#         assert p.dtype == itype
+#         assert p.shape == (A.shape[0],)
+#         assert is_valid_permutation(p)
+
+#     @pytest.mark.parametrize("aggressive", [True, False])
+#     def test_aggressive(self, A, aggressive):
+#         p = colamd(A, aggressive=aggressive)
+#         assert is_valid_permutation(p)
+
+
+# DENSE_THRESHOLDS = [None, 5, 2]
+
+
+# @pytest.mark.parametrize("dense_thresh", DENSE_THRESHOLDS)
+# def test_amd_with_dense_rows(dense_thresh):
+#     N = 1000
+#     rng = np.random.default_rng(56)
+#     A = sparse.random_array((N, N), density=0.001, format="lil", rng=rng)
+
+#     # Create a known number of dense rows above the threshold
+#     # thresh is actually dense_thresh * sqrt(N) == dense_thresh * 10
+#     # max(A[i] for i in range(N)) is ~ 5 for N = 1000, density = 0.001
+#     AMD_DEFAULT_DENSE = 10
+#     thresh = int(
+#         (dense_thresh if dense_thresh is not None else AMD_DEFAULT_DENSE) * np.sqrt(N)
+#     )
+
+#     N_dense_rows = 10  # arbitrary choice for number of dense rows
+#     N_elems = min(2 * thresh, N)  # arbitrary choice to ensure enough elements
+
+#     dense_row_idx = rng.choice(N, size=N_dense_rows, replace=False)
+#     col_idx = rng.choice(N, size=N_elems, replace=False)
+#     for i in dense_row_idx:
+#         # Ensure the row is dense enough
+#         A[i, col_idx] = rng.random(size=len(col_idx))
+
+#     A = A + A.T
+#     A = A.tocsc()
+#     p = colamd(A, dense_thresh=dense_thresh)
+
+#     assert is_valid_permutation(p)
+
+#     # Expect dense row at the end of the permutation, but maybe not in order
+#     assert_array_equal(np.sort(p[-N_dense_rows:]), np.sort(dense_row_idx))
+
+# # =============================================================================
+# # =============================================================================
