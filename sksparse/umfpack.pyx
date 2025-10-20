9c0024420a7b4f90a59434eb8ef4c884e646e685
Bernard Roesler <bernard.roesler@gmail.com>
2025-10-20 12:59:40 -0400

    MNT: Refactor UMFFactor.solve to use fused type for b, x.

--- sksparse/umfpack.pyx
+++ sksparse/umfpack.pyx
@@ -1147,133 +1147,96 @@ cdef class UMFFactor:
         # Allocate the output array
         x = np.empty_like(b, order='F')
 
-        if self._is_real:
-            self._solve_real(sys, A, b, x)
-        else:
-            self._solve_complex(sys, A, b, x)
+        self._solve(sys, A, b, x)
 
         if return_1D:
             return x[:, 0]
         else:
             return x
 
-    cdef void _solve_real(
+    def _solve(
         self,
         int sys,
         object A,
-        double[::1, :] b,
-        double[::1, :] x
+        value_t[::1, :] b,
+        value_t[::1, :] x
     ):
-        """Solve multiple RHS systems when `b` is real."""
+        """Solve multiple RHS systems."""
         cdef Py_ssize_t k
         cdef Py_ssize_t K = b.shape[1]
         for k in range(K):
-            self._solve_single_rhs_real(sys, A, b[:, k], x[:, k])
+            self._solve_single_rhs(sys, A, <double*>&b[0, k], <double*>&x[0, k])
 
-    cdef void _solve_complex(
+    cdef void _solve_single_rhs(
         self,
         int sys,
         object A,
-        double complex[::1, :] b,
-        double complex[::1, :] x
+        double* b_ptr,
+        double* x_ptr
     ):
-        """Solve multiple RHS systems when `b` is complex."""
-        cdef Py_ssize_t k
-        cdef Py_ssize_t K = b.shape[1]
-        for k in range(K):
-            self._solve_single_rhs_complex(sys, A, b[:, k], x[:, k])
-
-    cdef void _solve_single_rhs_real(
-        self,
-        int sys,
-        object A,
-        double[::1] b,
-        double[::1] x
-    ):
-        """Solve a single RHS system when RHS is real."""
+        """Solve a single RHS system."""
         # Pointers to the underlying arrays
         cdef np.ndarray indptr = A.indptr
         cdef np.ndarray indices = A.indices
         cdef np.ndarray data = A.data
-
-        cdef double *b_ptr = &b[0]
-        cdef double *x_ptr = &x[0]
 
         # Solve the system
-        if self._use_int32:
-            status = umfpack_di_solve(
-                sys,
-                <int32_t*>indptr.data,
-                <int32_t*>indices.data,
-                <double*>data.data,
-                x_ptr,
-                b_ptr,
-                self._numeric,
-                self._control.data,
-                self._info.data
-            )
+        if self._is_real:
+            if self._use_int32:
+                status = umfpack_di_solve(
+                    sys,
+                    <int32_t*>indptr.data,
+                    <int32_t*>indices.data,
+                    <double*>data.data,
+                    x_ptr,
+                    b_ptr,
+                    self._numeric,
+                    self._control.data,
+                    self._info.data
+                )
+            else:
+                status = umfpack_dl_solve(
+                    sys,
+                    <int64_t*>indptr.data,
+                    <int64_t*>indices.data,
+                    <double*>data.data,
+                    x_ptr,
+                    b_ptr,
+                    self._numeric,
+                    self._control.data,
+                    self._info.data
+                )
         else:
-            status = umfpack_dl_solve(
-                sys,
-                <int64_t*>indptr.data,
-                <int64_t*>indices.data,
-                <double*>data.data,
-                x_ptr,
-                b_ptr,
-                self._numeric,
-                self._control.data,
-                self._info.data
-            )
-
-        _handle_errors(status)
-
-    cdef void _solve_single_rhs_complex(
-        self,
-        int sys,
-        object A,
-        double complex[::1] b,
-        double complex[::1] x
-    ):
-        """Solve a single RHS system when RHS is complex."""
-        # Pointers to the underlying arrays
-        cdef np.ndarray indptr = A.indptr
-        cdef np.ndarray indices = A.indices
-        cdef np.ndarray data = A.data
-
-        # Cast to double* for real and imaginary parts in packed form
-        cdef double *b_ptr = <double*>&b[0]
-        cdef double *x_ptr = <double*>&x[0]
-
-        if self._use_int32:
-            status = umfpack_zi_solve(
-                sys,
-                <int32_t*>indptr.data,
-                <int32_t*>indices.data,
-                <double*>data.data,
-                NULL,
-                x_ptr,
-                NULL,
-                b_ptr,
-                NULL,
-                self._numeric,
-                self._control.data,
-                self._info.data
-            )
-        else:
-            status = umfpack_zl_solve(
-                sys,
-                <int64_t*>indptr.data,
-                <int64_t*>indices.data,
-                <double*>data.data,
-                NULL,
-                x_ptr,
-                NULL,
-                b_ptr,
-                NULL,
-                self._numeric,
-                self._control.data,
-                self._info.data
-            )
+            if self._use_int32:
+                status = umfpack_zi_solve(
+                    sys,
+                    <int32_t*>indptr.data,
+                    <int32_t*>indices.data,
+                    <double*>data.data,
+                    NULL,
+                    x_ptr,
+                    NULL,
+                    b_ptr,
+                    NULL,
+                    self._numeric,
+                    self._control.data,
+                    self._info.data
+                )
+            else:
+                status = umfpack_zl_solve(
+                    sys,
+                    <int64_t*>indptr.data,
+                    <int64_t*>indices.data,
+                    <double*>data.data,
+                    NULL,
+                    x_ptr,
+                    NULL,
+                    b_ptr,
+                    NULL,
+                    self._numeric,
+                    self._control.data,
+                    self._info.data
+                )
 
         _handle_errors(status)
 
