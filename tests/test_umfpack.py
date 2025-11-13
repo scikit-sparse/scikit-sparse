7d0299be5f35583c83c055098de4afc6207be147
Bernard Roesler <bernard.roesler@gmail.com>
2025-11-12 20:34:41 -0500

    TST: Test UMFPACK refactor with different itypes.

--- tests/test_umfpack.py
+++ tests/test_umfpack.py
@@ -249,11 +249,14 @@ test_As = [
 
 
 @pytest.mark.parametrize("copy", [False, True])
+@pytest.mark.parametrize("itype", ITYPES)
 @pytest.mark.parametrize("A", test_As)
-def test_refactor(A, copy):
+def test_refactor(A, itype, copy):
     atol = 1e-12 if A.dtype in (np.float64, np.complex128) else 1e-6
     A = A.copy()
     A.setdiag(A.diagonal() + 1.0)  # make non-singular
+    A.indptr = A.indptr.astype(itype)
+    A.indices = A.indices.astype(itype)
     f = umf_factor(A)
     assert_LU_equals_A(f, A, atol=atol)
     # Create a new matrix with the same sparsity pattern but different values
