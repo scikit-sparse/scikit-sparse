85d2d42fed79de4e37fa8879d0385d6d5c90f3db
Bernard Roesler <bernard.roesler@gmail.com>
2025-11-24 11:22:46 -0500

    TST: Test UMFFactor.solve RHS casting.

--- tests/test_umfpack.py
+++ tests/test_umfpack.py
@@ -418,6 +418,27 @@ def test_nearly_singular(davis_example_qr):
         f.solve(b)
 
 
+class TestRHSCasting:
+    @pytest.fixture
+    def example_system(self, davis_example_qr):
+        A = davis_example_qr
+        N = A.shape[0]
+        f = umf_factor(A)
+        expect_x = np.arange(1, N + 1, dtype=A.dtype)
+        b = A @ expect_x
+        return f, expect_x, b
+
+    def test_upcast_rhs(self, example_system):
+        f, expect_x, b = example_system
+        x = f.solve(b.astype(np.float16))
+        assert_allclose(x, expect_x, strict=True, rtol=1e-3)
+
+    def test_downcast_rhs(self, example_system):
+        f, expect_x, b = example_system
+        with pytest.raises(TypeError, match="Cannot safely cast"):
+            f.solve(b.astype(np.complex64))
+
+
 square_As = [
     A
     for dtype in DTYPES
